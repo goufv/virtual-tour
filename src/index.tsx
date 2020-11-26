eaec5d0151d78d539defaa03158a1e63ce2c1bdf
Tylor Sherman <tylorsherman@gmail.com>
2020-11-25 16:56:33 -0800

    Navigate to differet videos

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
-import React from "react"
-import { render } from "react-dom"
+// import React from "react"
+// import { render } from "react-dom"
 
 import('./index.scss')
 import map from 'url:./map.svg'
@@ -9,14 +9,16 @@ document.write(`
     <div class="stage">
       <div class="locations">
         <div class="map"></div>
+        <div class="list"></div>
       </div>
       <div class="player">
         <div class="instructions">
-          <h1>Be cool be cool</h1>
+          <h1>Choose a location</h1>
         </div>
         <div class="modal">
+          <div class="name"></div>
           <video id="video" playsinline preload>
-            <source src="http://s3.amazonaws.com/denimandsteel/ufv/IMG_8338.mov">
+            <source src="">
           </video>
         </div>
       </div>
@@ -24,30 +26,129 @@ document.write(`
   </div>`
 )
 
-const show = (e, item) => {
-  e.preventDefault();
-  if (item) {
-    item.classList.add('visited')
-  }
-  document.querySelector('.modal').classList.toggle('active')
-  if (document.querySelector('.modal').classList.contains('active')) {
-    document.querySelector('#video').play()
+const locations = [
+  {
+    id: 'building-1',
+    name: 'Building 1',
+    // video: 'https://ddwhdx18r9qya.cloudfront.net/ufv/IMG_9076_720.mp4',
+    video: 'https://ddwhdx18r9qya.cloudfront.net/ufv/IMG_9076_720_smaller.mp4',
+  },
+  {
+    id: 'building-2',
+    name: 'Building 2',
+    // video: 'https://ddwhdx18r9qya.cloudfront.net/ufv/IMG_9077_720.mp4',
+    video: 'https://ddwhdx18r9qya.cloudfront.net/ufv/IMG_9077_720_smaller.mp4',
+  },
+  {
+    id: 'building-3',
+    name: 'Building 3',
+    // video: 'https://ddwhdx18r9qya.cloudfront.net/ufv/IMG_9078_720.mp4',
+    video: 'https://ddwhdx18r9qya.cloudfront.net/ufv/IMG_9078_720_smaller.mp4',
+  },
+  {
+    id: 'building-4',
+    name: 'Building 4',
+    // video: 'https://ddwhdx18r9qya.cloudfront.net/ufv/IMG_9076_720.mp4',
+    video: 'https://ddwhdx18r9qya.cloudfront.net/ufv/IMG_9076_720_smaller.mp4',
+  },
+  {
+    id: 'building-5',
+    name: 'Building 5',
+    // video: 'https://ddwhdx18r9qya.cloudfront.net/ufv/IMG_9077_720.mp4',
+    video: 'https://ddwhdx18r9qya.cloudfront.net/ufv/IMG_9077_720_smaller.mp4',
+  },
+  {
+    id: 'building-6',
+    name: 'Building 6',
+    // video: 'https://ddwhdx18r9qya.cloudfront.net/ufv/IMG_9078_720.mp4',
+    video: 'https://ddwhdx18r9qya.cloudfront.net/ufv/IMG_9078_720_smaller.mp4',
+  },
+  {
+    id: 'building-7',
+    name: 'Building 7',
+    // video: 'https://ddwhdx18r9qya.cloudfront.net/ufv/IMG_9076_720.mp4',
+    video: 'https://ddwhdx18r9qya.cloudfront.net/ufv/IMG_9076_720_smaller.mp4',
+  },
+  {
+    id: 'building-8',
+    name: 'Building 8',
+    // video: 'https://ddwhdx18r9qya.cloudfront.net/ufv/IMG_9077_720.mp4',
+    video: 'https://ddwhdx18r9qya.cloudfront.net/ufv/IMG_9077_720_smaller.mp4',
+  },
+  {
+    id: 'building-9',
+    name: 'Building 9',
+    // video: 'https://ddwhdx18r9qya.cloudfront.net/ufv/IMG_9078_720.mp4',
+    video: 'https://ddwhdx18r9qya.cloudfront.net/ufv/IMG_9078_720_smaller.mp4',
+  },
+  {
+    id: 'building-10',
+    name: 'Building 10',
+    // video: 'https://ddwhdx18r9qya.cloudfront.net/ufv/IMG_9076_720.mp4',
+    video: 'https://ddwhdx18r9qya.cloudfront.net/ufv/IMG_9076_720_smaller.mp4',
+  },
+  {
+    id: 'building-11',
+    name: 'Building 11',
+    // video: 'https://ddwhdx18r9qya.cloudfront.net/ufv/IMG_9077_720.mp4',
+    video: 'https://ddwhdx18r9qya.cloudfront.net/ufv/IMG_9077_720_smaller.mp4',
+  },
+]
+
+const $videoPlayer = document.querySelector('#video')
+const $modal = document.querySelector('.modal')
+
+const toggleVideo = (e, item, location) => {
+  if (item.classList.contains('active')) {
+    console.log('hideing')
+    hideVideo(e);
   }
   else {
-    document.querySelector('#video').pause()
-    document.querySelector('#video').currentTime = 0
+    console.log('showing')
+    showVideo(e, item, location);
+  }
+}
+
+const showVideo = (e, item, location) => {
+  e.preventDefault()
+  const activeLocation = document.querySelector('.building.active')
+  if (activeLocation) {
+    activeLocation.classList.remove('active')
+  }
+  item.classList.add('visited')
+  item.classList.add('active')
+  const source = $videoPlayer.querySelector('source')
+  source.setAttribute('src', location.video)
+  $modal.querySelector('.name').innerHTML = location.name
+  $videoPlayer.load();
+  $videoPlayer.currentTime = 0
+  $modal.classList.add('active')
+  $videoPlayer.play()
+}
+
+const hideVideo = (e) => {
+  e.preventDefault()
+  $videoPlayer.pause()
+  $modal.classList.remove('active')
+  const activeLocation = document.querySelector('.building.active')
+  if (activeLocation) {
+    activeLocation.classList.remove('active')
   }
 }
 
 const setup = (svgData) => {
   document.querySelector("#ufv-map-root .map").insertAdjacentHTML('beforeend', svgData);
 
-  [].forEach.call(document.querySelectorAll('.building'), (item) => {
-    item.addEventListener('click', (e) => { show(e, item) })
-    item.addEventListener('touchstart', (e) => { show(e, item) })
-    document.querySelector('.modal').addEventListener('click', show)
-    document.querySelector('.modal').addEventListener('touchstart', show)
+  locations.forEach((location) => {
+    console.log(location)
+    const item = document.getElementById(location.id)
+    item.addEventListener('click', (e) => { toggleVideo(e, item, location) })
+    item.addEventListener('touchstart', (e) => { toggleVideo(e, item, location) })
   });
+  // [].forEach.call(document.querySelectorAll('.building'), (item) => {
+  // });
+  $modal.addEventListener('click', hideVideo)
+  $modal.addEventListener('touchstart', hideVideo)
 
 }
 
