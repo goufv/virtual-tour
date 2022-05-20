04cd6415a98e960a8c19e32bf3b8c8ee3d0bbf22
Tylor Sherman <tylorsherman@gmail.com>
2022-05-20 16:34:54 -0700

    Doc on convert script

--- convert-videos.sh
+++ convert-videos.sh
@@ -15,3 +15,8 @@ do
   # Generate WebM
   ffmpeg -i $i -c:v libvpx -qmin 0 -qmax 25 -crf 4 -b:v 1M -vf scale=720:-2 -threads 0 dist/${i%"mp4"}webm
 done
+
+
+
+# ffmpeg -i WEBSITE_CapuletAnimation-FINALREV_2.mp4 -c:v libx264 -pix_fmt yuv420p -profile:v baseline -level 3.0 -crf 22 -preset veryslow -vf scale=1600:-2 -movflags +faststart -threads 0 WEBSITE_CapuletAnimation-FINALREV_2_1600.mp4
+# ffmpeg -i WEBSITE_CapuletAnimation-FINALREV_2.mp4  -c:v libvpx -qmin 0 -qmax 25 -crf 4 -b:v 1M -vf scale=1600:-2 -c:a libvorbis -threads 0 -pass 2 WEBSITE_CapuletAnimation-FINALREV_2_1600.webm
